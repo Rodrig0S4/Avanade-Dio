585a0e892db32152432b86ae789c29d88ce0feb8
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-26 19:12:54 -0300

    initial project

---
+++ IntroDesafios/teste.js
@@ -0,0 +1,18 @@
+// var y = 0;
+// var x = 1; 
+
+// var z = 2;
+
+
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;
+
+PRECO = preco - desconto;
+
+// function soma (a, b) {
+//     return a + b; // 8
+// }
+
+// soma(3, 5);
