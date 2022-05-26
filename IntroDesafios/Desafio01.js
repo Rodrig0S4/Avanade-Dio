6e669d4d734320f3b7f33cfcaea573c88c77da68
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-26 15:05:41 -0300

    atualização de informações

---
+++ IntroDesafios/Desafio01.js
@@ -0,0 +1,11 @@
+// A função "gets" é implementada dentro do sistema para ler as entradas (inputs) dos dados;
+// A função "print" imprime a saída (output) de dados e já pula uma linha("\n")
+// Abaixo segue um exemplo de código que você poude ou não utilizar.
+
+var a = parseInt(gets());
+var b = parseInt(gets());
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio.
+
+var soma = a + b;         
+print("soma = " + soma);
