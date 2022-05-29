1f7fccad28c6f6958bdedb833677a45415a1494a
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-29 14:04:20 -0300

    Functions

---
+++ MassificandoJavaScript/aula 5/assets/scripts.js
@@ -0,0 +1,10 @@
+// function funcao() {
+//     console.log('Tudo certo por aqui!')
+// }
+// funcao();
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem('Tudo certo', 'por aqui');
