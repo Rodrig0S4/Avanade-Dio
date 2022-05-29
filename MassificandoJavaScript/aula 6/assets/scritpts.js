5ceb303aff1574bbd0d6b39070a506179c5d9a63
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-29 16:19:06 -0300

    Digging deeper into functions

---
+++ MassificandoJavaScript/aula 6/assets/scritpts.js
@@ -0,0 +1,20 @@
+//  Tipos de função
+
+// Declarativa
+// function funcao() {
+// console.log('Sou a mensagem de uma função declarativa!')
+// }
+// funcao();
+
+// Expressão de função 
+// Com nomeação
+// var funcao = function() {
+//     console.log ("Sou uma mensagem de função de expressão!")
+// }
+// funcao();
+
+// Arrow function 
+// var funcao = () => {
+//     console.log("Sou uma arrow function!")
+// }
+// funcao();
