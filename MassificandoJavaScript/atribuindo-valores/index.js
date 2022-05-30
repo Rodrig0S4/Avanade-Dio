a8e82d32250370b3000b9f8a3d907b6f4b6daac5
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-29 22:46:11 -0300

    Variables

---
+++ MassificandoJavaScript/atribuindo-valores/index.js
@@ -0,0 +1,15 @@
+// numberOne = 1;
+
+// console.log(numberOne + 2);
+
+// var numberOne; 
+
+var firstName = 'João';
+var lastName = 'Souza';
+
+if (firstName === 'João') {
+    var firstName = "Pedro";
+    let lastName = "Silva";
+}
+
+console.log(firstName, lastName);
