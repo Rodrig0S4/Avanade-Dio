5ed6bd58e45da42d8672998b9b693950a8188566
Rodrig0S4 <sdearaujo.rodrigo@gmail.com>
2022-05-29 23:19:25 -0300

    let, var and const

---
+++ MassificandoJavaScript/aulasJS/2-atribuindo-valores/let_var&const.js
@@ -0,0 +1,5 @@
+                                  // var                              //   const                                 // let
+//   escopo            global ou local                         bloco                                  bloco
+// redeclarar                 sim                                    não                                     não
+// reatribuir                  sim                                    não                                     sim 
+// hoisting                    sim                                    não                                     não      
